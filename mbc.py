e18f5475cedf8766666300974a34716ef05b3709
wldn9312 <wldn9312@daum.net>
2024-08-19 12:35:05 +0900

    20240819

---
+++ mbc.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+query = "안세영"
+search_url = "https://search.imbc.com/news?qt={query}"
+
+response = requests.get(search_url)
+soup = BeautifulSoup(response.text,"html.parser")
+
+image_tage = soup.find("ul", class_= "list-type news-list")
+lis = image_tage.find_all("ul")
+
+for img_tah in lis:
+    img_url = image_tag.find("img").get("src")
+    img_data = requests.get(f"https{img_url}").content
+    
